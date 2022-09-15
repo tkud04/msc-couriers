8d813d135721e10a05598ce883c42eba44db8be1
Tobi Kudayisi <kudayisitobi@gmail.com>
2022-09-15 06:40:52 +0100

    added database config

--- config/database.php
+++ config/database.php
@@ -43,15 +43,15 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        //mysql://bc440733bcf608:2a4f6b64@us-cdbr-east-05.cleardb.net/heroku_2fc4a19de09e920?reconnect=true
+        //mysql://be56739ebacb59:96d0e01a@us-cdbr-east-03.cleardb.com/heroku_b91ba749191c3e8?reconnect=true
         'mysql' => [
             'driver' => 'mysql',
             //'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'us-cdbr-east-05.cleardb.net'),
+            'host' => env('DB_HOST', 'us-cdbr-east-03.cleardb.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'heroku_2fc4a19de09e920'),
-            'username' => env('DB_USERNAME', 'bc440733bcf608'),
-            'password' => env('DB_PASSWORD', '2a4f6b64'),
+            'database' => env('DB_DATABASE', 'heroku_b91ba749191c3e8'),
+            'username' => env('DB_USERNAME', 'be56739ebacb59'),
+            'password' => env('DB_PASSWORD', '96d0e01a'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'unix_socket' => env('DB_SOCKET', ''),
