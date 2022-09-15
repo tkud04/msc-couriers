ffe0f8bcac2a9293b09f1255c0819bc5d05b8728
Tobi Kudayisi <kudayisitobi@gmail.com>
2022-09-15 07:14:33 +0100

    added database config

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -402,7 +402,7 @@ $subject = $data['subject'];
 		   
 		   function getTNum()
 		   {
-			   return "DGS".rand(1999,9999999);
+			   return "MSC".rand(1999,9999999);
 		   }
 		   
 		     function addTracking($data)
